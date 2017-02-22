626d8d2bd594431feb1fafc6efd04c0971230295
Feng Li <fengliplatform@gmail.com>
2017-02-22 18:09:37 +0000

    more modification to aligh with CodeBook, ReadMe

--- run_analysis.R
+++ run_analysis.R
@@ -5,30 +5,23 @@
 ########
 # 0. Download dataset
 ########
-
-# create data dir if necessary
-setwd("~/course/ds_coursera/course3")
-if(!file.exists("./data")) {
-  dir.create("./data")
-}
 # download dataset zip file
+print("0. Download and unzip dataset...")
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile="./data/project.zip", method="curl")
-list.files("./data")
+download.file(fileUrl, destfile="./project.zip", method="curl")
 
 # unzip file and rename created directory
-setwd("~/course/ds_coursera/course3/data")
 unzip("./project.zip")
 file.rename(from="UCI HAR Dataset", to="UCI_HAR_Dataset")
-setwd("~/course/ds_coursera/course3/data/UCI_HAR_Dataset")
-list.files("./")
+setwd("./UCI_HAR_Dataset")
+print("0. Download and unzip dataset...done")
 
 ########
 # 1. read train and test dataset.
 # This is to do step 1 from project instruction: 1. Merges the training and the test 
 # sets to create one data set.
 ######## 
-
+print("1. Read train and test data set and combine them to a data frame...")
 # read train dataset as txt file
 trainfile <- "./train/X_train.txt"
 lines <- readLines(trainfile)
@@ -39,7 +32,7 @@ lines <- strsplit(lines, split=" ")
 lines <- unlist(lines)
 lines.m <-matrix(lines, ncol=561, byrow=TRUE) # There are 561 features based on README.txt
 train.df <- as.data.frame(lines.m)
-
+print("Finished reading train data set, read test data set... ")
 # read test dataset as txt file
 testfile <- "./test/X_test.txt"
 lines <- readLines(testfile)
@@ -53,12 +46,14 @@ test.df <- as.data.frame(lines.m)
 
 # Merges the training and the test sets to create one data set.
 full.df <- rbind(train.df, test.df)
+print("1. Read train and test data set and combine them to a data frame...done")
 
 ########
 # 2. Extract required columns to be new data frame
 # This is to do step 2 from project instruction: 2. Extracts only the measurements on 
 # the mean and standard deviation for each measurement.
 ######## 
+print("2. Extract only mean and std variables to create a new data frame...")
 library(dplyr)
 # read feature names
 featurefile <- "./features.txt"
@@ -73,12 +68,14 @@ feature.mean.sd.names <- grep("mean()|std()", feature.names,value=TRUE)
 
 # create new data frame only includes required columns
 full.df.mean.sd <- select(full.df, feature.mean.sd.position)
+print("2. Extract only mean and std mesurements to create a new data frame...done")
 
 ########
 # 3. Handle activity names and subjects to data frame created from above
 # This is to do step 3 from project instruction: 3. Uses descriptive activity names to 
 # name the activities in the data set 
 ########
+print("3. Attach activity names and subject to the data frame...")
 # read and add activity label to dataset
 # read activity labels for train dataset
 train.activity.file <- "./train/y_train.txt"
@@ -112,21 +109,24 @@ activity.name.df <- read.table(activity.name.file, header=FALSE)
 names(activity.name.df) <- c("label","activity.name")
 
 # merge dataset and activity names dataset. This will give each 
-#record a human readable activity description 
+# record a human readable activity description 
 full.df.mean.sd <- merge(full.df.mean.sd, activity.name.df, by.x="activity.label", by.y="label")
 full.df.mean.sd <- full.df.mean.sd[,-1]
+print("3. Attach activity names and subject to the data frame...done")
 
 ########
 # 4. Give column names
 # This is to do step 4 from project instruction: 4. Appropriately labels 
 # the data set with descriptive variable names. 
 ########
+print("4. Label the data set with descriptive variable names...")
 # use feature.mean.sd.names created in section 2 to name columns
-feature.mean.sd.names2 <- gsub("-", "", feature.mean.sd.names)
+feature.mean.sd.names2 <- gsub("-", ".", feature.mean.sd.names)
 feature.mean.sd.names2 <- c(feature.mean.sd.names2, "subject")
 feature.mean.sd.names2 <- c(feature.mean.sd.names2, "activity.name")
 feature.mean.sd.names2
 names(full.df.mean.sd) <- feature.mean.sd.names2
+print("4. Label the data set with descriptive variable names...done")
 
 ########
 # 5. create new dataset
@@ -134,6 +134,7 @@ names(full.df.mean.sd) <- feature.mean.sd.names2
 # creates a second, independent tidy data set with the average of each variable 
 # for each activity and each subject. 
 ########
+print("5. Create a second, independent tidy data set with the average of each variable for each activity and each subject...")
 # Temporarily change activity name from factor to character 
 full.df.mean.sd$activity.name <- as.character(full.df.mean.sd$activity.name)
 
@@ -150,7 +151,7 @@ new.df <- full.df.mean.sd %>%
   summarize_each(funs(mean(., na.rm=TRUE)))
 
 # create dataset file to submit
-setwd("~/course/ds_coursera/course3")
-write.table(new.df, file="meandata.txt", sep=" ", row.names=FALSE)
+write.table(new.df, file="../meandata.txt", sep=" ", row.names=FALSE)
+print("5. Create a second, independent tidy data set with the average of each variable for each activity and each subject...done")
 
 
